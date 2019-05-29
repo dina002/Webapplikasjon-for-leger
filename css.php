659454e9d3ade5d3a0618876a439d20339b60e72
Simon Torstensen <torstensen.simon@gmail.com>
2019-05-29 14:41:48 +0200

    Update css.php

--- css.php
+++ css.php
@@ -1,7 +1,7 @@
 <?php
   $host="localhost";
   $user="web-prg11v09";
-  $password="WwgKm3Wy";
+  $password="PASSWORD";
   $database="web-prg11v09";
   $db=mysqli_connect($host, $user, $password, $database) or die("Ikke kontakt med database-server");
 
